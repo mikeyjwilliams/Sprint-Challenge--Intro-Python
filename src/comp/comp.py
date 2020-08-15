2d23f94986c9d852b50bd20781260422aff24470
Mikey Williams <manicdesigner@gmail.com>
2020-08-15 15:04:36 -0400

    think got swuar root of age...

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -61,11 +61,14 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [(human.name.upper()+'',''+str(human.age)) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [sum(human.age * human.age for human in humans)]
 print(h)
+
+
+print(humans)
